4368dd868907bd4ec79ea0a63a7347731f8825dc
imdong-geun <dg600123@gmail.com>
2022-06-06 21:46:12 +0900

    유효한 괄호 문제

---
+++ Algorithm_interview/20_valid_parentheses.py
@@ -0,0 +1,18 @@
+# 20) 유효한 괄호
+def isvalid(s: str) -> bool:
+    stack = []
+    table = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for char in s:
+        if char not in table:
+            stack.append(char)
+        elif not stack or table[char] != stack.pop():
+            return False
+    return len(stack) == 0
+
+q = '(){}[]'
+isvalid(q)
