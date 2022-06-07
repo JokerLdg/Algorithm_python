80164f1bf65947dadb70ed745995b45133a7c5af
imdong-geun <dg600123@gmail.com>
2022-06-07 23:02:20 +0900

    중복 문자 제거 문제

---
+++ Algorithm_interview/21_remove_duplicate_letters.py
@@ -0,0 +1,35 @@
+# 21) 중복 문자 제거
+import collections
+
+# 재귀로 진행
+def remove_duplicate_letters_recursion(s: str) -> str:
+    # 집합으로 정렬
+    for char in sorted(set(s)):
+        suffix = s[s.index(char):]
+        # 전체 집합과 접미사 집합이 일치할때 진행
+        if set(s) == set(suffix):
+            return char + remove_duplicate_letters_recursion(suffix.replace(char, ''))
+    return ''
+
+# 스택으로 진행
+def remove_duplicate_letters_stack(s: str) -> str:
+    counter, seen, stack = collections.Counter(s), set(), []
+
+    for char in s:
+        counter[char] -= 1
+
+        if char in seen:
+            continue
+
+        # 뒤에 붙일 문자가 남아 있다면 스택에서 제거
+        while stack and char < stack[-1] and counter[stack[-1]] > 0:
+            seen.remove(stack.pop())
+
+        stack.append(char)
+        seen.add(char)
+    return ''.join(stack)
+
+
+q = 'bcabc'
+print(remove_duplicate_letters_recursion(q))
+print(remove_duplicate_letters_stack(q))
